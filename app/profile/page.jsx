db0f4cf592118f3f8dc69dce1258e8e654a4d3f6
Liz <claireliang2012@gmail.com>
2023-07-13 22:23:10 +0200

    Finish delete post function on profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,7 +27,24 @@ const MyProfile = () => {
     router.push(`/update-post?id=${post._id}`);
   };
 
-  const handleDelete = () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this post?");
+    // This confirm prompt is built into the browser API
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/post/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        // Filter the post that has been deleted, and update myPosts
+        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
