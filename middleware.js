ad92b3541d3efe9f7e05c5b3b18b3445d5026cbc
Liz <claireliang2012@gmail.com>
2023-08-15 09:37:25 +0200

    organise middleware code

--- middleware.js
+++ middleware.js
@@ -5,41 +5,41 @@ export const middleware = async (req) => {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname } = req.nextUrl;
 
-  const generateRandomString = (length) => {
-    let result = "";
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
-  };
+  // const generateRandomString = (length) => {
+  //   let result = "";
+  //   const characters =
+  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  //   const charactersLength = characters.length;
+  //   let counter = 0;
+  //   while (counter < length) {
+  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  //     counter += 1;
+  //   }
+  //   return result;
+  // };
 
-  const randomState = generateRandomString(16);
+  // const randomState = generateRandomString(16);
 
-  const params = {
-    scope: [
-      "user-read-email",
-      "user-read-private",
-      "user-library-read",
-      "user-library-modify",
-      "user-read-currently-playing",
-      "user-read-recently-played",
-      "user-modify-playback-state",
-      "streaming",
-    ].join(","),
-    response_type: "code",
-    redirect_uri: `${process.env.BASE_URL}/api/auth/callback/spotify`,
-    client_id: process.env.SPOTIFY_CLIENT_ID,
-    state: randomState,
-  };
+  // const params = {
+  //   scope: [
+  //     "user-read-email",
+  //     "user-read-private",
+  //     "user-library-read",
+  //     "user-library-modify",
+  //     "user-read-currently-playing",
+  //     "user-read-recently-played",
+  //     "user-modify-playback-state",
+  //     "streaming",
+  //   ].join(","),
+  //   response_type: "code",
+  //   redirect_uri: `${process.env.BASE_URL}/api/auth/callback/spotify`,
+  //   client_id: process.env.SPOTIFY_CLIENT_ID,
+  //   state: randomState,
+  // };
 
-  const LOGIN_URL =
-    "https://accounts.spotify.com/authorize?" +
-    new URLSearchParams(params).toString();
+  // const LOGIN_URL =
+  //   "https://accounts.spotify.com/authorize?" +
+  //   new URLSearchParams(params).toString();
 
   if (pathname.includes("/api/auth") || token) return NextResponse.next();
   if (!token) return NextResponse.redirect(new URL("/", req.url));
