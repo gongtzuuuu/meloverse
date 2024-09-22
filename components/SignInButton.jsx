fbb66ca5aac49511aa814aeb3fce0b4e2e26291f
lizzy <lizzy@jgallop.com>
2024-09-22 20:43:23 +0800

    Push router to the homepage when logged out.

--- components/SignInButton.jsx
+++ components/SignInButton.jsx
@@ -1,16 +1,28 @@
 'use client';
-
+import { useRouter } from 'next/navigation';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import useUserStore from '@store/auth-store';
 
 const SignInButton = () => {
   const session = useSession();
+  const router = useRouter();
+  const setLogin = useUserStore((state) => state.login);
+  const setLogout = useUserStore((state) => state.logout);
+
   const handleSignIn = () => {
     signIn('spotify', { callbackUrl: '/' });
+    setLogin(session.user.id);
+  };
+
+  const handleSignOut = () => {
+    signOut();
+    setLogout();
+    router.push('/');
   };
   return (
     <div>
       {session.data && session.data.user ? (
-        <button type="button" onClick={signOut} className="outline_btn">
+        <button type="button" onClick={handleSignOut} className="outline_btn">
           Logout
         </button>
       ) : (
